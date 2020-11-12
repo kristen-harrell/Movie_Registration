7a2f098cf877d538c9308c4d8351843e1b3ea7fd
Kristen Harrell <harrell.kristen@gmail.com>
2020-11-12 17:33:15 -0500

    first commit

---
+++ Movie_Registration/Controllers/RegistrationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie_Registration.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Registration.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult RegistrationForm()
+        {
+            return View();
+        }
+
+        public IActionResult RegisteredMovies(Movie movie)
+        {
+
+            if (ModelState.IsValid)
+            {
+                return View("RegisteredMovies", movie);
+            }
+            else
+            {
+                return View("RegistrationForm", movie);
+            }
+        }
+
+    }
+}
---
+++ Movie_Registration/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movie_Registration.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage ="The maximum number of characters is 50")]
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        [Range(1880, 2020, ErrorMessage ="The earliest year you can enter is 1880" )]
+        public int Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+
+        //getting this ready so that I can put these in a table or on a list
+        //public Movie(int id, string title, string genre, int year, string actors, string directors )
+        //{
+        //    ID = id;
+        //    Title = title;
+        //    Genre = genre;
+        //    Year = year;
+        //    Actors = actors;
+        //    Directors = directors;
+        //}
+    }
+}
